fec3c011298bf6d120b27a01d0d4c10e46dcef20
Konstantin <teffal@mail.ru>
2017-03-25 14:06:25 +0300

    Next try

--- Home_task_2.3.py
+++ Home_task_2.3.py
@@ -2,20 +2,22 @@
 import json
 import yaml
 import os
-
 # Ver. 2017.03.21
+
 
 def file_cook_book(my_file_path):
     if my_file_path.endswith('.json'):
-        with open('Book_cook.json', 'r', encoding='utf-8') as f:
+        with open(my_file_path, 'r', encoding='utf-8') as f:
             return json.load(f)
     else:
-        with open('Book_cook.yml', 'r', encoding='utf-8') as f:
+        with open(my_file_path, 'r', encoding='utf-8') as f:
             return yaml.load(f)
+
 
 def get_shop_list_by_dishes(dishes, person_count, my_file_path):
     shop_list = {}
     for dish in dishes:
+        print(file_cook_book(my_file_path))
         for ingridient in file_cook_book(my_file_path)[dish]:
             new_shop_list_item = dict(ingridient)
             new_shop_list_item['quantity'] *= person_count
@@ -25,9 +27,11 @@ def get_shop_list_by_dishes(dishes, person_count, my_file_path):
                 shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
 
+
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
         print('{ingredient_name}: {quantity} {measure} '.format(**shop_list_item))
+
 
 def create_shop_list():
     my_file_path = input('Enter the path to your file (default path: Book_cook.json):')
